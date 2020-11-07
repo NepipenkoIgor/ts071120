99aacba7a558912e013d3f5bf9b8abddab27b07a
Igor Nepipenko <inepipenko@muellerone.com>
2020-11-07 11:57:03 +0200

    Feature: fix base file [ref:no-ref]

--- base.ts
+++ base.ts
@@ -1,32 +1,32 @@
-Object.defineProperty(window, "MySweetApp",
-    {value: "v1.0.0", readonly: false});
+Object.defineProperty(window, 'MySweetApp',
+    {value: 'v1.0.0', writable: true});
 
 function deliveryMethod() {
     // TODO some logic
-    return "overnight";
+    return 'overnight';
 }
 
-function shipWeight() {
-    return parseInt((document.getElementById('weight')/* @type {HTMLDivElement} */).innerHTML);
+function shipWeight(): number {
+    const el: HTMLDivElement = document.getElementById('weight') as HTMLDivElement;
+    // if (el === null) {
+    //     return 0;
+    // }
+    return parseInt(el.innerHTML, 10);
 }
 
-/**
- * @param {(string | string[])} emailAddr
- */
-function sendUpdates(emailAddr) {
-    /**
-     * @param {string} addr
-     */
-    function sendEmail(addr) {
-        console.log(`Shipping to ${addr} via ${deliveryMethod() | "standard"} delivery`);
+function sendUpdates(emailAddr: string | string[]) {
+    function sendEmail(addr: string) {
+        // tslint:disable-next-line:no-console
+        console.log(`Shipping to ${addr} via ${deliveryMethod() || 'standard'} delivery`);
 
         if (shipWeight() > 100) {
-            console.log("WARNING: Oversize package");
+            // tslint:disable-next-line:no-console
+            console.log('WARNING: Oversize package');
         }
     }
 
-    if (emailAddr.length) {
-        emailAddr.forEach((idx, val) => {
+    if (Array.isArray(emailAddr)) {
+        emailAddr.forEach((val) => {
             sendEmail(val.trim());
         });
     } else {
